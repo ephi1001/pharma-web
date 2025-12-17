1cc3a53b9010b386778fe90e75f9c01457127945
ephi1001 <eliasephrata@gmail.com>
2025-12-17 09:00:46 +0300

    delete backend

--- backend/routes/contactRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const contactController = require('../controller/contactController');
-
-// POST /api/contact-messages
-router.post('/contact-messages', contactController.createContactMessage);
-
-module.exports = router;
